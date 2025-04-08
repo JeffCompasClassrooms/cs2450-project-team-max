0c7ec378c280f2b6af845b06058390dd29c84568
Maxwell Schultz <maxdawg2006@gmail.com>
2025-04-08 17:32:42 -0600

    im so done

--- tests/test_friend_requests.py
+++ tests/test_friend_requests.py
@@ -75,7 +75,7 @@ class TestLikeButton(unittest.TestCase):
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "navbar-toggler"))).click()
         print('clicked')
 
-        wait.until(driver.find_element((By.CSS_SELECTOR, "input[type='submit'][value='Logout']"))).click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='submit'][value='Logout']"))).click()
         WebDriverWait(driver, 10).until(EC.url_changes(old_url))
 
         old_url = driver.current_url
