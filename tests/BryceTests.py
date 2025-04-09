81efca5fcdfb2919b70cb89934479bfa76ca81a3
Maxwell Schultz <maxdawg2006@gmail.com>
2025-04-09 15:28:28 -0600

    test12

--- tests/BryceTests.py
+++ tests/BryceTests.py
@@ -81,7 +81,9 @@ class TestLikeButton(unittest.TestCase):
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "navbar-toggler"))).click()
         print('clicked')
         time.sleep(1)   
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit'][value='Logout']"))).click()
+        logout = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit'][value='Logout']")))
+        print('logout found')
+        logout.click()
         WebDriverWait(driver, 10).until(EC.url_changes(old_url))
         print("[PASSED] - test1 logged out.")
 
